954025b4d9e5a65ca82f1e7e045b7071567b98e0
Rumeysa Saglam <117765158+ROzenSaglam@users.noreply.github.com>
2023-09-05 22:01:28 +0300

    initial commit

---
+++ screens/BlankScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ScreenContainer, withTheme } from '@draftbit/ui';
+import { useWindowDimensions } from 'react-native';
+
+const BlankScreen = props => {
+  const dimensions = useWindowDimensions();
+
+  const { theme } = props;
+
+  return <ScreenContainer hasSafeArea={false} scrollable={false} />;
+};
+
+export default withTheme(BlankScreen);
